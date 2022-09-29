34450e525b6af498a52bd420b1171d8b06aa1282
brandonhanks <br3andon@gmail.com>
2022-09-29 12:47:50 -0400

    Update plugin.rb
    added name metadata

--- plugin.rb
+++ plugin.rb
@@ -1,2 +1,4 @@
+# name: discourse-xorigin
+
 Rails.application.config.action_dispatch.default_headers.clear()
 Rails.application.config.action_dispatch.default_headers.merge!({'Content-Security-Policy' => 'frame-ancestors *', 'X-Frame-Options' => 'ALLOWALL'})
